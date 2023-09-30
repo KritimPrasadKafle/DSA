0dfe2e979850d00bd7bccd315d1fbd4d5b320bdf
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-09-30 15:27:30 +0530

    Create RemoveDuplicatesinArray.java

---
+++ Array/RemoveDuplicates.java
@@ -0,0 +1,103 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {1,1,2,2,2,3,3};
+        int k = removeDuplicates(arr);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] arr) {
+        HashSet < Integer > set = new HashSet < > ();
+        for (int i = 0; i < arr.length; i++) {
+            set.add(arr[i]);
+        }
+        int k = set.size();
+        int j = 0;
+        for (int x: set) {
+            arr[j++] = x;
+        }
+        return k;
+    }
+}
+
+
+
+
+from typing import List
+
+def removeDuplicates(arr: List[int]) -> int:
+    st = set()
+    for i in range(len(arr)):
+        st.add(arr[i])
+    k = len(st)
+    j = 0
+    for x in st:
+        arr[j] = x
+        j += 1
+    return k
+
+
+if __name__ == "__main__":
+    arr = [1, 1, 2, 2, 2, 3, 3]
+    k = removeDuplicates(arr)
+    print("The array after removing duplicate elements is ")
+    for i in range(k):
+        print(arr[i], end=" ")
+
+
+//   Time complexity: O(n*log(n))+O(n)
+
+// Space Complexity: O(n) 
+
+
+  
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+       int arr[] = {1,1,2,2,2,3,3};
+        int k = removeDuplicates(arr);
+        System.out.println("The array after removing duplicate elements is ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    static int removeDuplicates(int[] arr) {
+        int i = 0;
+        for (int j = 1; j < arr.length; j++) {
+            if (arr[i] != arr[j]) {
+                i++;
+                arr[i] = arr[j];
+            }
+        }
+        return i + 1;
+    }
+}
+
+
+
+from typing import List
+
+def removeDuplicates(arr: List[int]) -> int:
+    i = 0
+    for j in range(1, len(arr)):
+        if arr[i] != arr[j]:
+            i += 1
+            arr[i] = arr[j]
+    return i + 1
+
+
+if __name__ == "__main__":
+    arr = [1, 1, 2, 2, 2, 3, 3]
+    k = removeDuplicates(arr)
+    print("The array after removing duplicate elements is ")
+    for i in range(k):
+        print(arr[i], end=" ")
+
+
+
+  Time Complexity: O(N)
+
+Space Complexity: O(1)
