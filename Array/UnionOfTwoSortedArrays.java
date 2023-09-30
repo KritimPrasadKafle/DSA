409cbbf7e8415682c8ddeb78ff9e68a97096bce4
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-09-30 22:32:36 +0530

    Create UnionOfTwoSortedArrays.java

---
+++ Array/UnionOfTwoSortedArrays.java
@@ -0,0 +1,223 @@
+
+import java.util.*;
+
+class TUF{
+static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+  HashMap <Integer,Integer > freq=new HashMap<>();
+  ArrayList<Integer> Union=new ArrayList<>();
+  for (int i = 0; i < n; i++)
+    freq.put(arr1[i],freq.getOrDefault(arr1[i],0)+1);
+    
+  for (int i = 0; i < m; i++)
+    freq.put(arr2[i],freq.getOrDefault(arr2[i],0)+1);
+  for (int it: freq.keySet())
+    Union.add(it);
+  return Union;
+}
+
+public static void main(String args[]) {
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+  System.out.println("Union of arr1 and arr2 is ");
+  for (int val: Union)
+    System.out.print(val+" ");
+}
+}
+
+
+
+def find_union(arr1, arr2):
+    freq = {}
+    union = []
+    
+    for num in arr1:
+        freq[num] = freq.get(num, 0) + 1
+    
+    for num in arr2:
+        freq[num] = freq.get(num, 0) + 1
+    
+    for num in freq:
+        union.append(num)
+    
+    return union
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr2 = [2, 3, 4, 4, 5, 11, 12]
+
+union = find_union(arr1, arr2)
+
+print("Union of arr1 and arr2 is:")
+for num in union:
+    print(num, end=" ")
+
+
+
+
+//       Time Compleixty : O( (m+n)log(m+n) ) . Inserting a key in map takes logN times, where N is no of elements in map. At max map can store m+n elements {when there are no common elements and elements in arr,arr2 are distntict}. So Inserting m+n th element takes log(m+n) time. Upon approximation across insertion of all elements in worst it would take O((m+n)log(m+n) time.
+
+// Using HashMap also takes the same time, On average insertion in unordered_map takes O(1) time but sorting the union vector takes O((m+n)log(m+n))  time. Because at max union vector can have m+n elements.
+
+// Space Complexity : O(m+n) {If Space of Union ArrayList is considered} 
+
+// O(1) {If Space of union ArrayList is not considered}
+
+
+      
+import java.util.*;
+
+class TUF{
+static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+  HashSet <Integer> s=new HashSet<>();
+  ArrayList < Integer > Union=new ArrayList<>();
+  for (int i = 0; i < n; i++)
+    s.add(arr1[i]);
+  for (int i = 0; i < m; i++)
+    s.add(arr2[i]);
+  for (int it: s)
+    Union.add(it);
+  return Union;
+}
+public static void main(String args[]) {
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+  System.out.println("Union of arr1 and arr2 is ");
+  for (int val: Union)
+    System.out.print(val+" ");
+}
+}
+
+
+
+
+def find_union(arr1, arr2):
+    s = set()
+    union = []
+    
+    for num in arr1:
+        s.add(num)
+    
+    for num in arr2:
+        s.add(num)
+    
+    for num in s:
+        union.append(num)
+    
+    return union
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr2 = [2, 3, 4, 4, 5, 11, 12]
+
+union = find_union(arr1, arr2)
+
+print("Union of arr1 and arr2 is:")
+print(*union)
+
+
+// Time Compleixty : O( (m+n)log(m+n) ) . Inserting an element in a set takes logN time, where N is no of elements in the set. At max set can store m+n elements {when there are no common elements and elements in arr,arr2 are distntict}. So Inserting m+n th element takes log(m+n) time. Upon approximation across inserting all elements in worst, it would take O((m+n)log(m+n) time.
+
+// Using HashSet also takes the same time, On average insertion in unordered_set takes O(1) time but sorting the union vector takes O((m+n)log(m+n))  time. Because at max union vector can have m+n elements.
+
+// Space Complexity : O(m+n) {If Space of Union ArrayList is considered} 
+
+// O(1) {If Space of union ArrayList is not considered}
+
+
+  
+
+
+import java.util.*;
+
+class TUF{
+static ArrayList<Integer> FindUnion(int arr1[], int arr2[], int n, int m) {
+   int i = 0, j = 0; // pointers
+  ArrayList<Integer > Union=new ArrayList<>(); // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr1[i])
+        Union.add(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.get(Union.size()-1) != arr2[j])
+        Union.add(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.get(Union.size()-1) != arr1[i])
+      Union.add(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.get(Union.size()-1) != arr2[j])
+      Union.add(arr2[j]);
+    j++;
+  }
+  return Union;
+}
+public static void main(String args[]) {
+  int n = 10, m = 7;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {2, 3, 4, 4, 5, 11, 12};
+  ArrayList<Integer> Union = FindUnion(arr1, arr2, n, m);
+  System.out.println("Union of arr1 and arr2 is ");
+  for (int val: Union)
+    System.out.print(val+" ");
+}
+}
+
+
+
+
+
+
+
+def find_union(arr1, arr2):
+    i, j = 0, 0  # Pointers
+    union = []  # Union list
+
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] <= arr2[j]:  # Case 1 and 2
+            if len(union) == 0 or union[-1] != arr1[i]:
+                union.append(arr1[i])
+            i += 1
+        else:  # Case 3
+            if len(union) == 0 or union[-1] != arr2[j]:
+                union.append(arr2[j])
+            j += 1
+
+    while i < len(arr1):  # If any elements left in arr1
+        if union[-1] != arr1[i]:
+            union.append(arr1[i])
+        i += 1
+
+    while j < len(arr2):  # If any elements left in arr2
+        if union[-1] != arr2[j]:
+            union.append(arr2[j])
+        j += 1
+
+    return union
+
+
+arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+arr2 = [2, 3, 4, 4, 5, 11, 12]
+
+union = find_union(arr1, arr2)
+
+print("Union of arr1 and arr2 is:")
+print(*union)
+
+
+
+//   Time Complexity: O(m+n), Because at max i runs for n times and j runs for m times. When there are no common elements in arr1 and arr2 and all elements in arr1, arr2 are distinct. 
+
+// Space Complexity : O(m+n) {If Space of Union ArrayList is considered} 
+
+// O(1) {If Space of union ArrayList is not considered}
