97ab694530f52caafc776c08f1a5936d042eb787
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-09-30 14:52:22 +0530

    Create LargestElementinArray.java

---
+++ Array/LargestElementinArray.java
@@ -0,0 +1,93 @@
+
+import java.util.*;
+public class tuf {
+ 
+  public static void main(String args[]) {
+ 
+    int arr1[] =  {2,5,1,3,0};
+    System.out.println("The Largest element in the array is: " + sort(arr1));
+   
+    int arr2[] =  {8,10,5,7,9};
+    System.out.println("The Largest element in the array is: " + sort(arr2));
+  }
+  static int sort(int arr[]) {
+    Arrays.sort(arr);
+    return arr[arr.length - 1];
+  }
+}
+
+
+
+
+from typing import List
+def sortArr(arr: List[int]) -> int:
+    arr.sort()
+    return arr[-1]
+if __name__ == "__main__":
+    arr1 = [2, 5, 1, 3, 0]
+    arr2 = [8, 10, 5, 7, 9]
+    print("The Largest element in the array is:", sortArr(arr1))
+    print("The Largest element in the array is:", sortArr(arr2))
+
+
+//   Time Complexity: O(N*log(N))
+
+// Space Complexity: O(n)
+
+
+
+  
+import java.util.*;
+public class tuf {
+ 
+  public static void main(String args[]) {
+ 
+    int arr1[] =  {2,5,1,3,0};
+    System.out.println("The Largest element in the array is: "+findLargestElement(ar
+    r1));
+ 
+    int arr2[] =  {8,10,5,7,9};
+    System.out.println("The Largest element in the array is: "+findLargestElement(ar
+    r2));
+  }
+  static int findLargestElement(int arr[]) {
+    int max= arr[0];
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] > max) {
+        max= arr[i];
+      }
+    }
+    return max;
+  }
+}
+
+
+
+def findLargestElement(arr, n):
+
+
+    max = arr[0]
+    for i in range(0, n):
+        if (max < arr[i]):
+            max = arr[i]
+    return max
+
+
+
+
+if __name__ == "__main__":
+    arr1 = [2, 5, 1, 3, 0]
+    n = 5
+    max = findLargestElement(arr1, n)
+    print("The largest element in the array is:", max)
+
+
+    arr2 = [8, 10, 5, 7, 9]
+    n = 5
+    max = findLargestElement(arr2, n)
+    print("The largest element in the array is:", max)
+
+
+//   Time Complexity: O(N)
+
+// Space Complexity: O(1)
