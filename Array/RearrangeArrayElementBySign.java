112f7dce7ae860cdd15d81743a12e0d34b37f21a
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-03 00:06:32 +0530

    Create RearrangeArrayElementBySign.java

---
+++ Array/RearrangeArrayElementBySign.java
@@ -0,0 +1,255 @@
+import java.util.*;
+
+class TUF{
+
+public static int[] RearrangebySign(int[] A, int n){
+    
+  // Define 2 vectors, one for storing positive 
+  // and other for negative elements of the array.
+  ArrayList<Integer> pos=new ArrayList<>();
+  ArrayList<Integer> neg=new ArrayList<>();
+  
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(A[i]>0) pos.add(A[i]);
+      else neg.add(A[i]);
+  }
+  
+  // Positives on even indices, negatives on odd.
+  for(int i=0;i<n/2;i++){
+      
+      A[2*i] = pos.get(i);
+      A[2*i+1] = neg.get(i);
+  }
+
+ 
+  return A;
+}    
+
+public static void main(String args[]) 
+{
+  // Array Initialisation.
+  int n = 4;
+  int A[]= {1,2,-4,-5};
+
+
+  int[]ans= RearrangebySign(A,n);
+  
+  for (int i = 0; i < n; i++) {
+    System.out.print(ans[i]+" ");
+  }
+
+}
+}
+
+
+
+def rearrange_by_sign(A):
+    # Define 2 lists, one for storing positive and other for negative elements of the array.
+    pos = []
+    neg = []
+  
+    # Segregate the array into positives and negatives.
+    for i in range(len(A)):
+        if A[i] > 0:
+            pos.append(A[i])
+        else:
+            neg.append(A[i])
+  
+    # Positives on even indices, negatives on odd.
+    for i in range(len(pos)):
+        A[2 * i] = pos[i]
+    for i in range(len(neg)):
+        A[2 * i + 1] = neg[i]
+  
+    return A
+
+# Array Initialisation.
+A = [1, 2, -4, -5]
+ans = rearrange_by_sign(A)
+
+for i in range(len(ans)):
+    print(ans[i], end=" ")
+
+
+
+
+//   Time Complexity: O(N+N/2) { O(N) for traversing the array once for segregating positives and negatives and another O(N/2) for adding those elements alternatively to the array, where N = size of the array A}.
+
+// Space Complexity:  O(N/2 + N/2) = O(N) { N/2 space required for each of the positive and negative element arrays, where N = size of the array A}.
+
+
+
+  import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Array Initialization.
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, -4, -5));
+        ArrayList<Integer> ans = RearrangebySign(A);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+    public static ArrayList<Integer> RearrangebySign(ArrayList<Integer> A) {
+        int n = A.size();
+
+        // Define array for storing the ans separately.
+        ArrayList<Integer> ans = new ArrayList<>(Collections.nCopies(n, 0));
+
+        // positive elements start from 0 and negative from 1.
+        int posIndex = 0, negIndex = 1;
+        for (int i = 0; i < n; i++) {
+
+            // Fill negative elements in odd indices and inc by 2.
+            if (A.get(i) < 0) {
+                ans.set(negIndex, A.get(i));
+                negIndex += 2;
+            }
+          // Fill positive elements in even indices and inc by 2.
+            else {
+                ans.set(posIndex, A.get(i));
+                posIndex += 2;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// Time Complexity: O(N) { O(N) for traversing the array once and substituting positives and negatives simultaneously using pointers, where N = size of the array A}.
+
+// Space Complexity:  O(N) { Extra Space used to store the rearranged elements separately in an array, where N = size of array A}.
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Array Initialization
+        int n = 6;
+        ArrayList<Integer> A = new ArrayList<>(Arrays.asList(1, 2, -4, -5, 3, 4));
+
+        ArrayList<Integer> ans = RearrangebySign(A, n);
+
+        for (int i = 0; i < ans.size(); i++) {
+            System.out.print(ans.get(i) + " ");
+        }
+    }
+
+    public static ArrayList<Integer> RearrangebySign(ArrayList<Integer> A, int n) {
+        // Define 2 ArrayLists, one for storing positive 
+        // and other for negative elements of the array.
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+
+        // Segregate the array into positives and negatives.
+        for (int i = 0; i < n; i++) {
+            if (A.get(i) > 0)
+                pos.add(A.get(i));
+            else
+                neg.add(A.get(i));
+        }
+
+        // If positives are lesser than the negatives.
+        if (pos.size() < neg.size()) {
+
+            // First, fill array alternatively till the point 
+            // where positives and negatives are equal in number.
+            for (int i = 0; i < pos.size(); i++) {
+                A.set(2 * i, pos.get(i));
+                A.set(2 * i + 1, neg.get(i));
+            }
+
+            // Fill the remaining negatives at the end of the array.
+            int index = pos.size() * 2;
+            for (int i = pos.size(); i < neg.size(); i++) {
+                A.set(index, neg.get(i));
+                index++;
+            }
+        }
+
+        // If negatives are lesser than the positives.
+        else {
+            // First, fill array alternatively till the point 
+            // where positives and negatives are equal in number.
+            for (int i = 0; i < neg.size(); i++) {
+                A.set(2 * i, pos.get(i));
+                A.set(2 * i + 1, neg.get(i));
+            }
+
+            // Fill the remaining positives at the end of the array.
+            int index = neg.size() * 2;
+            for (int i = neg.size(); i < pos.size(); i++) {
+                A.set(index, pos.get(i));
+                index++;
+            }
+        }
+        return A;
+    }
+}
+
+
+
+def RearrangebySign(A, n):
+    # Define 2 lists, one for storing positive 
+    # and other for negative elements of the array.
+    pos = []
+    neg = []
+    
+    # Segregate the array into positives and negatives.
+    for i in range(n):
+        if A[i] > 0:
+            pos.append(A[i])
+        else:
+            neg.append(A[i])
+    
+    # If positives are lesser than the negatives.
+    if len(pos) < len(neg):
+        # First, fill array alternatively till the point 
+        # where positives and negatives are equal in number.
+        for i in range(len(pos)):
+            A[2*i] = pos[i]
+            A[2*i+1] = neg[i]
+        
+        # Fill the remaining negatives at the end of the array.
+        index = len(pos)*2
+        for i in range(len(neg)-len(pos)):
+            A[index] = neg[len(pos)+i]
+            index += 1
+    
+    # If negatives are lesser than the positives.
+    else:
+        # First, fill array alternatively till the point 
+        # where positives and negatives are equal in number.
+        for i in range(len(neg)):
+            A[2*i] = pos[i]
+            A[2*i+1] = neg[i]
+        
+        # Fill the remaining positives at the end of the array.
+        index = len(neg)*2
+        for i in range(len(pos)-len(neg)):
+            A[index] = pos[len(neg)+i]
+            index += 1
+    
+    return A
+
+# Array initialization
+n = 6
+A = [1, 2, -4, -5, 3, 4]
+
+ans = RearrangebySign(A, n)
+
+for i in range(len(ans)):
+    print(ans[i], end=" ")
+
+
+
+//   Time Complexity: O(2*N) { The worst case complexity is O(2*N) which is a combination of O(N) of traversing the array to segregate into neg and pos array and O(N) for adding the elements alternatively to the main array}.
+
+// Explanation: The second O(N) is a combination of O(min(pos, neg)) + O(leftover elements). There can be two cases: when only positive or only negative elements are present, O(min(pos, neg)) + O(leftover) = O(0) + O(N), and when equal no. of positive and negative elements are present, O(min(pos, neg)) + O(leftover) = O(N/2) + O(0). So, from these two cases, we can say the worst-case time complexity is O(N) for the second part, and by adding the first part we get the total complexity of O(2*N).
+
+// Space Complexity:  O(N/2 + N/2) = O(N) { N/2 space required for each of the positive and negative element arrays, where N = size of the array A}.
