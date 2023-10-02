5e166bd733d0927a8daa7bce242ef3d9d6482576
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-02 23:59:29 +0530

    Create StockBuyAndSell.java

---
+++ Array/StockBuyAndSell.java
@@ -0,0 +1,96 @@
+
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int arr[] = {7,1,5,3,6,4};
+        int maxPro = maxProfit(arr);
+        System.out.println("Max profit is: " + maxPro);
+    }
+
+    static int maxProfit(int[] arr) {
+        int maxPro = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    maxPro = Math.max(arr[j] - arr[i], maxPro);
+                }
+            }
+        }
+        return maxPro;
+    }
+}
+
+
+
+from typing import List
+
+def maxProfit(arr: List[int]) -> int:
+    maxPro = 0
+    n = len(arr)
+
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            if arr[j] > arr[i]:
+                maxPro = max(arr[j] - arr[i], maxPro)
+
+
+    return maxPro
+
+
+if __name__ == "__main__":
+    arr = [7, 1, 5, 3, 6, 4]
+    maxPro = maxProfit(arr)
+    print("Max profit is: ", maxPro)
+
+
+
+//   Time complexity: O(n^2)
+
+// Space Complexity: O(1)
+
+
+
+  
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+    int arr[] = {7,1,5,3,6,4};
+
+    int maxPro = maxProfit(arr);
+    System.out.println("Max profit is: " + maxPro);
+
+    }
+    static int maxProfit(int[] arr) {
+    int maxPro = 0;
+    int minPrice = Integer.MAX_VALUE;
+    for (int i = 0; i < arr.length; i++) {
+        minPrice = Math.min(minPrice, arr[i]);
+        maxPro = Math.max(maxPro, arr[i] - minPrice);
+    }
+    return maxPro;
+    }
+}
+
+
+
+
+def maxProfit(arr):
+    maxPro = 0
+    minPrice = float('inf')
+    for i in range(len(arr)):
+        minPrice = min(minPrice, arr[i])
+        maxPro = max(maxPro, arr[i] - minPrice)
+    return maxPro
+
+arr = [7, 1, 5, 3, 6, 4]
+maxPro = maxProfit(arr)
+print("Max profit is:", maxPro)
+
+
+
+//   Time complexity: O(n)
+
+// Space Complexity: O(1)
