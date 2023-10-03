1809d4c50b54db1eb6c00bd8c569140640c3dad0
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-03 22:07:08 +0530

    Create SetMatrixZeros.java

---
+++ Array/SetMatrixZeros.java
@@ -0,0 +1,352 @@
+import java.util.*;
+
+public class tUf {
+    static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i) {
+        // set all non-zero elements as -1 in the row i:
+        for (int j = 0; j < m; j++) {
+            if (matrix.get(i).get(j) != 0) {
+                matrix.get(i).set(j, -1);
+            }
+        }
+    }
+
+    static void markCol(ArrayList<ArrayList<Integer>> matrix, int n, int m, int j) {
+        // set all non-zero elements as -1 in the col j:
+        for (int i = 0; i < n; i++) {
+            if (matrix.get(i).get(j) != 0) {
+                matrix.get(i).set(j, -1);
+            }
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    markRow(matrix, n, m, i);
+                    markCol(matrix, n, m, j);
+                }
+            }
+        }
+        // Finally, mark all -1 as 0:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == -1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
+
+        System.out.println("The Final matrix is: ");
+        for (ArrayList<Integer> row : ans) {
+            for (Integer ele : row) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+def markRow(matrix, n, m, i):
+    # set all non-zero elements as -1 in the row i:
+    for j in range(m):
+        if matrix[i][j] != 0:
+            matrix[i][j] = -1
+
+def markCol(matrix, n, m, j):
+    # set all non-zero elements as -1 in the col j:
+    for i in range(n):
+        if matrix[i][j] != 0:
+            matrix[i][j] = -1
+
+def zeroMatrix(matrix, n, m):
+    # Set -1 for rows and cols
+    # that contains 0. Don't mark any 0 as -1:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                markRow(matrix, n, m, i)
+                markCol(matrix, n, m, j)
+    
+    # Finally, mark all -1 as 0:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == -1:
+                matrix[i][j] = 0
+    
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
+
+
+
+//   Time Complexity: O((N*M)*(N + M)) + O(N*M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Reason: Firstly, we are traversing the matrix to find the cells with the value 0. It takes O(N*M). Now, whenever we find any such cell we mark that row and column with -1. This process takes O(N+M). So, combining this the whole process, finding and marking, takes O((N*M)*(N + M)).
+// Another O(N*M) is taken to mark all the cells with -1 as 0 finally.
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+
+  
+import java.util.*;
+
+public class tUf {
+    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        int[] row = new int[n]; // row array
+        int[] col = new int[m]; // col array
+
+        // Traverse the matrix:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark ith index of row wih 1:
+                    row[i] = 1;
+
+                    // mark jth index of col wih 1:
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Finally, mark all (i, j) as 0
+        // if row[i] or col[j] is marked with 1.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix.get(i).set(j, 0);
+                }
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
+
+        System.out.println("The Final matrix is: ");
+        for (ArrayList<Integer> row : ans) {
+            for (Integer ele : row) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+def zeroMatrix(matrix, n, m):
+    row = [0] * n  # row array
+    col = [0] * m  # col array
+
+    # Traverse the matrix:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                # mark ith index of row wih 1:
+                row[i] = 1
+
+                # mark jth index of col wih 1:
+                col[j] = 1
+
+    # Finally, mark all (i, j) as 0
+    # if row[i] or col[j] is marked with 1.
+    for i in range(n):
+        for j in range(m):
+            if row[i] or col[j]:
+                matrix[i][j] = 0
+
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
+
+
+//   Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Reason: We are traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+
+// Space Complexity: O(N) + O(M), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Reason: O(N) is for using the row array and O(M) is for using the col array.
+
+
+  
+import java.util.*;
+
+public class Main {
+    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+        // int[] row = new int[n]; --> matrix[..][0]
+        // int[] col = new int[m]; --> matrix[0][..]
+
+        int col0 = 1;
+        // step 1: Traverse the matrix and
+        // mark 1st row & col accordingly:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix.get(i).get(j) == 0) {
+                    // mark i-th row:
+                    matrix.get(i).set(0, 0);
+
+                    // mark j-th column:
+                    if (j != 0)
+                        matrix.get(0).set(j, 0);
+                    else
+                        col0 = 0;
+                }
+            }
+        }
+
+        // Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix.get(i).get(j) != 0) {
+                    // check for col & row:
+                    if (matrix.get(i).get(0) == 0 || matrix.get(0).get(j) == 0) {
+                        matrix.get(i).set(j, 0);
+                    }
+                }
+            }
+        }
+
+        //step 3: Finally mark the 1st col & then 1st row:
+        if (matrix.get(0).get(0) == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix.get(0).set(j, 0);
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix.get(i).set(0, 0);
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+
+        int n = matrix.size();
+        int m = matrix.get(0).size();
+
+        ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
+
+        System.out.println("The Final matrix is: ");
+        for (ArrayList<Integer> row : ans) {
+            for (Integer ele : row) {
+                System.out.print(ele + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+def zeroMatrix(matrix, n, m):
+    # int row[n] = {0}; --> matrix[..][0]
+    # int col[m] = {0}; --> matrix[0][..]
+
+    col0 = 1
+    # step 1: Traverse the matrix and
+    # mark 1st row & col accordingly:
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                # mark i-th row:
+                matrix[i][0] = 0
+
+                # mark j-th column:
+                if j != 0:
+                    matrix[0][j] = 0
+                else:
+                    col0 = 0
+
+    # Step 2: Mark with 0 from (1,1) to (n-1, m-1):
+    for i in range(1, n):
+        for j in range(1, m):
+            if matrix[i][j] != 0:
+                # check for col & row:
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+
+    #step 3: Finally mark the 1st col & then 1st row:
+    if matrix[0][0] == 0:
+        for j in range(m):
+            matrix[0][j] = 0
+    if col0 == 0:
+        for i in range(n):
+            matrix[i][0] = 0
+
+    return matrix
+
+
+if __name__ == "__main__":
+	matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+	n = len(matrix)
+	m = len(matrix[0])
+	ans = zeroMatrix(matrix, n, m)
+
+	print("The Final matrix is:")
+	for row in ans:
+	    for ele in row:
+	        print(ele, end=" ")
+	    print()
+
+
+
+
+//   Time Complexity: O(2*(N*M)), where N = no. of rows in the matrix and M = no. of columns in the matrix.
+// Reason: In this approach, we are also traversing the entire matrix 2 times and each traversal is taking O(N*M) time complexity.
+
+// Space Complexity: O(1) as we are not using any extra space.
