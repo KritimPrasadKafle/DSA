73947680bee71581cf3cc2fe8a08e8465c664196
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-06 23:27:50 +0530

    Create LowerBound.java

---
+++ Binary Search/LowerBound.java
@@ -0,0 +1,113 @@
+
+import java.util.*;
+
+public class tUf {
+
+    public static int lowerBound(int []arr, int n, int x) {
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= x) {
+                // lower bound found:
+                return i;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = 5, x = 9;
+        int ind = lowerBound(arr, n, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+}
+
+
+
+
+def lowerBound(arr: [int], n: int, x: int) -> int:
+    for i in range(n):
+        if arr[i] >= x:
+            #lower bound found
+            return i
+    return n
+
+if __name__ == "__main__":
+    arr = [3, 5, 8, 15, 19]
+    n = 5
+    x = 9
+    ind = lowerBound(arr, n, x)
+    print("The lower bound is the index:", ind)
+
+
+
+//   Time Complexity: O(N), where N = size of the given array.
+// Reason: In the worst case, we have to travel the whole array. This is basically the time complexity of the linear search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
+
+
+  
+import java.util.*;
+
+public class tUf {
+
+    public static int lowerBound(int []arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 15, 19};
+        int n = 5, x = 9;
+        int ind = lowerBound(arr, n, x);
+        System.out.println("The lower bound is the index: " + ind);
+    }
+} 
+
+
+
+
+def lowerBound(arr: [int], n: int, x: int) -> int:
+    low = 0
+    high = n - 1
+    ans = n
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] >= x:
+            ans = mid
+            # look for smaller index on the left
+            high = mid - 1
+        else:
+            low = mid + 1  # look on the right
+
+    return ans
+
+if __name__ == "__main__":
+    arr = [3, 5, 8, 15, 19]
+    n = 5
+    x = 9
+    ind = lowerBound(arr, n, x)
+    print("The lower bound is the index:", ind)
+
+
+
+//   Time Complexity: O(logN), where N = size of the given array.
+// Reason: We are basically using the Binary Search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
+
