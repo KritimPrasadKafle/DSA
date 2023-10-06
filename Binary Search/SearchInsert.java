535fdb1f9a5484f212ff73a7d87777600266d045
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-06 23:30:22 +0530

    Create SearchInsert.java

---
+++ Binary Search/SearchInsert.java
@@ -0,0 +1,61 @@
+public class Main {
+
+    public static int searchInsert(int [] arr, int x) {
+        int n = arr.length; // size of the array
+        int low = 0, high = n - 1;
+        int ans = n;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // maybe an answer
+            if (arr[mid] >= x) {
+                ans = mid;
+                //look for smaller index on the left
+                high = mid - 1;
+            } else {
+                low = mid + 1; // look on the right
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 7};
+        int x = 6;
+        int ind = searchInsert(arr, x);
+        System.out.println("The index is: " + ind);
+    }
+}
+
+
+
+def searchInsert(arr: [int], x: int) -> int:
+    n = len(arr)  # size of the array
+    low = 0
+    high = n - 1
+    ans = n
+
+    while low <= high:
+        mid = (low + high) // 2
+        # maybe an answer
+        if arr[mid] >= x:
+            ans = mid
+            # look for smaller index on the left
+            high = mid - 1
+        else:
+            low = mid + 1  # look on the right
+
+    return ans
+
+if __name__ == "__main__":
+    arr = [1, 2, 4, 7]
+    x = 6
+    ind = searchInsert(arr, x)
+    print("The index is:", ind)
+
+
+
+//   Time Complexity: O(logN), where N = size of the given array.
+// Reason: We are basically using the Binary Search algorithm.
+
+// Space Complexity: O(1) as we are using no extra space.
