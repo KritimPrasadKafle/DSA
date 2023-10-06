8f26366298f121065eed716aae8f2adb2bde2a3b
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-06 23:26:07 +0530

    Update BinarySearch.java

--- Binary Search/BinarySearch.java
+++ Binary Search/BinarySearch.java
@@ -15,3 +15,102 @@ class Solution {
         return -1;
     }
 }
+
+
+
+
+def binarySearch(nums: [int], target: int):
+    n = len(nums)  # size of the array
+    low = 0
+    high = n - 1
+
+    # Perform the steps:
+    while low <= high:
+        mid = (low + high) // 2
+        if nums[mid] == target:
+            return mid
+        elif target > nums[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+if __name__ == "__main__":
+    a = [3, 4, 6, 7, 9, 12, 16, 17]
+    target = 6
+    ind = binarySearch(a, target)
+    if ind == -1:
+        print("The target is not present.")
+    else:
+        print("The target is at index:", ind)
+
+
+// TC:   In the algorithm, in every step, we are basically dividing the search space into 2 equal halves. This is actually equivalent to dividing the size of the array by 2, every time. After a certain number of divisions, the size will reduce to such an extent that we will not be able to divide that anymore and the process will stop. The number of total divisions will be equal to the time complexity.
+
+// Let’s derive the number of divisions mathematically,
+
+// If a number n can be divided by 2 for x times:
+// 	2^x = n
+// Therefore, x = logn (base is 2)
+// So the overall time complexity is O(logN), where N = size of the given array.
+
+
+    
+import java.util.*;
+
+public class tUf {
+
+    public static int binarySearch(int[] nums, int low, int high, int target) {
+        if (low > high) return -1; //Base case.
+
+        // Perform the steps:
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return mid;
+        else if (target > nums[mid])
+            return binarySearch(nums, mid + 1, high, target);
+        return binarySearch(nums, low, mid - 1, target);
+    }
+
+    public static int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    public static void main(String[] args) {
+        int[] a = {3, 4, 6, 7, 9, 12, 16, 17};
+        int target = 6;
+        int ind = search(a, target);
+        if (ind == -1)
+            System.out.println("The target is not present.");
+        else
+            System.out.println("The target is at index: " + ind);
+    }
+}
+
+
+
+
+def binarySearch(nums: [int], low: int, high: int, target: int):
+    if low > high:
+        return -1  # Base case
+    
+    # Perform the steps:
+    mid = (low + high) // 2
+    if nums[mid] == target:
+        return mid
+    elif target > nums[mid]:
+        return binarySearch(nums, mid + 1, high, target)
+    return binarySearch(nums, low, mid - 1, target)
+
+def search(nums: [int], target: int):
+    return binarySearch(nums, 0, len(nums) - 1, target)
+
+if __name__ == "__main__":
+    a = [3, 4, 6, 7, 9, 12, 16, 17]
+    target = 6
+    ind = search(a, target)
+    if ind == -1:
+        print("The target is not present.")
+    else:
+        print("The target is at index:", ind) 
+
+
