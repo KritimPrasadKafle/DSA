37edfb3e8ddb7c2629ca34a60d4e6050cddb69b8
Kritim Prasad Kafle <124390253+KritimPrasadKafle@users.noreply.github.com>
2023-10-09 22:16:44 +0530

    Create Search in Rotated Sorted Array II.java

---
+++ Binary Search/Search in Rotated Sorted Array II.java
@@ -0,0 +1,158 @@
+import java.util.*;
+
+public class tUf {
+    public static boolean searchInARotatedSortedArrayII(int []arr, int k) {
+        int n = arr.length; // size of the array.
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == k) return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int k = 3;
+        boolean ans = searchInARotatedSortedArrayII(arr, k);
+        if (ans == false)
+            System.out.println("Target is not present.");
+        else
+            System.out.println("Target is present in the array.");
+    }
+}
+
+
+
+
+from typing import *
+def searchInARotatedSortedArrayII(arr : List[int], k : int) -> bool:
+    for num in arr:
+        if num == k:
+            return True
+    return False
+
+if __name__ == "__main__":
+    arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6]
+    k = 3
+    ans = searchInARotatedSortedArrayII(arr, k)
+    if not ans:
+        print("Target is not present.")
+    else:
+        print("Target is present in the array.")
+
+
+  Time Complexity: O(N), N = size of the given array.
+Reason: We have to iterate through the entire array to check if the target is present in the array.
+
+Space Complexity: O(1)
+Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
+
+
+
+  
+import java.util.*;
+
+public class tUf {
+    public static boolean searchInARotatedSortedArrayII(int []arr, int k) {
+        int n = arr.length; // size of the array.
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //if mid points the target
+            if (arr[mid] == k) return true;
+
+            //Edge case:
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] <= k && k <= arr[mid]) {
+                    //element exists:
+                    high = mid - 1;
+                } else {
+                    //element does not exist:
+                    low = mid + 1;
+                }
+            } else { //if right part is sorted:
+                if (arr[mid] <= k && k <= arr[high]) {
+                    //element exists:
+                    low = mid + 1;
+                } else {
+                    //element does not exist:
+                    high = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+        int k = 3;
+        boolean ans = searchInARotatedSortedArrayII(arr, k);
+        if (ans == false)
+            System.out.println("Target is not present.");
+        else
+            System.out.println("Target is present in the array.");
+    }
+}
+
+
+
+
+from typing import *
+def searchInARotatedSortedArrayII(arr : List[int], k : int) -> bool:
+    n = len(arr)  # size of the array
+    low, high = 0, n - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        # if mid points to the target
+        if arr[mid] == k:
+            return True
+
+        # Edge case:
+        if arr[low] == arr[mid] and arr[mid] == arr[high]:
+            low += 1
+            high -= 1
+            continue
+
+        # if left part is sorted
+        if arr[low] <= arr[mid]:
+            if arr[low] <= k <= arr[mid]:
+                # element exists
+                high = mid - 1
+            else:
+                # element does not exist
+                low = mid + 1
+        else:  # if right part is sorted
+            if arr[mid] <= k <= arr[high]:
+                # element exists
+                low = mid + 1
+            else:
+                # element does not exist
+                high = mid - 1
+
+    return False
+
+if __name__ == "__main__":
+    arr = [7, 8, 1, 2, 3, 3, 3, 4, 5, 6]
+    k = 3
+    ans = searchInARotatedSortedArrayII(arr, k)
+    if not ans:
+        print("Target is not present.")
+    else:
+        print("Target is present in the array.")
+
+
+
+  Time Complexity: O(logN) for the best and average case. O(N/2) for the worst case. Here, N = size of the given array.
+Reason: In the best and average scenarios, the binary search algorithm is primarily utilized and hence the time complexity is O(logN). However, in the worst-case scenario, where all array elements are the same but not the target (e.g., given array = {3, 3, 3, 3, 3, 3, 3}), we continue to reduce the search space by adjusting the low and high pointers until they intersect. This worst-case situation incurs a time complexity of O(N/2).
+
+Space Complexity: O(1)
+Reason: We have not used any extra data structures, this makes space complexity, even in the worst case as O(1).
